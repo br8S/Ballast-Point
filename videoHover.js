4c4f4f4f09271d29c963e80b977a78ff5073c5b6
br8S <56180503+br8S@users.noreply.github.com>
2021-02-23 18:53:01 -0800

    added js to for video play functionality

--- videoHover.js
+++ videoHover.js
@@ -19,3 +19,13 @@ document.querySelector('.play-button').addEventListener('mouseleave', () => {
     document.querySelector('.play-button').style.backgroundImage = "url('images/play-red.png')";
     document.querySelector('.complete-video-bg').style.opacity = "70%";
 })
+
+document.querySelector('.complete-video-bg').addEventListener('click', () => {
+    document.querySelector('.complete-video-vid').style.visibility = "visible";
+    document.querySelector('.complete-video-vid').play();
+})
+
+document.querySelector('.play-button').addEventListener('click', () => {
+    document.querySelector('.complete-video-vid').style.visibility = "visible";
+    document.querySelector('.complete-video-vid').play();
+})
