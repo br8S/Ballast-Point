c4bd7cfcbf51c20229bea8ac1acd442deb45c522
br8S <56180503+br8S@users.noreply.github.com>
2021-02-25 10:45:07 -0800

    using .children to access nested elements within mobile nav

--- mobileNav.js
+++ mobileNav.js
@@ -18,30 +18,40 @@ document.querySelector('.burger').addEventListener('click', () => {
         document.querySelectorAll('.mobile-nav-links').forEach(link => {
             link.style.transform = "translate(0, 2000%)";
         })
+
         document.querySelector('.beers-drop-nav').style.display = "none";
         document.querySelector('.beers-drop-nav').classList.remove('mobile-nav-bg');
+        document.querySelector('.locations-drop-nav').style.display = "none";
+        document.querySelector('.locations-drop-nav').classList.remove('mobile-nav-bg');
+        document.querySelector('.about-drop-nav').style.display = "none";
+        document.querySelector('.about-drop-nav').classList.remove('mobile-nav-bg');
         mobileOn = false;
     }
 })
 
-document.querySelectorAll('.mobile-nav-links').forEach(function (link, index) {
-    if (index === 0) {
-        link.addEventListener('click', () => {
+var mobile_nav_children = document.querySelector('.mobile-nav').children;
+
+for (var i = 0; i < mobile_nav_children.length; i++) {
+    if (i === 0) {
+        mobile_nav_children[i].addEventListener('click', () => {
             document.querySelector('.beers-drop-nav').style.display = "flex";
             document.querySelector('.mobile-nav').style.display = "none";
             document.querySelector('.beers-drop-nav').classList.add('mobile-nav-bg');
         })
     }
-    if (index === 1) {
-        link.addEventListener('click', () => {
-            console.log("locations")
+    if (i === 1) {
+        mobile_nav_children[i].addEventListener('click', () => {
+            document.querySelector('.locations-drop-nav').style.display = "flex";
+            document.querySelector('.mobile-nav').style.display = "none";
+            document.querySelector('.locations-drop-nav').classList.add('mobile-nav-bg');
         })
     }
-    if (index === 2) {
-        link.addEventListener('click', () => {
-            console.log("locations")
+    if (i === 2) {
+        mobile_nav_children[i].addEventListener('click', () => {
+            document.querySelector('.about-drop-nav').style.display = "flex";
+            document.querySelector('.mobile-nav').style.display = "none";
+            document.querySelector('.about-drop-nav').classList.add('mobile-nav-bg');
         })
     }
-
-})
+}
 
