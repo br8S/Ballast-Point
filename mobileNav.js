a4621aa341fe01cee9b853b39db20292322a0ad6
br8S <56180503+br8S@users.noreply.github.com>
2021-02-24 16:48:13 -0800

    added mobile navigation scroll up animation

--- mobileNav.js
+++ mobileNav.js
@@ -1,10 +1,22 @@
+var mobileOn = false;
+
 document.querySelector('.burger').addEventListener('click', () => {
-    if (document.querySelector('.mobile-nav').style.display == 'flex') {
-        document.querySelector('.mobile-nav').style.display = "none";
-        document.querySelector('.burger').style.backgroundImage = "url('images/burger.png')";
-    }
-    else{
-        document.querySelector('.mobile-nav').style.display = "flex";
+    if(mobileOn == false){
         document.querySelector('.burger').style.backgroundImage = "url('images/cross.png')";
+        document.querySelector('.mobile-nav').classList.add('mobile-nav-bg');
+        document.querySelectorAll('.mobile-nav-links').forEach(link => {
+            link.style.transform = "translate(0, 0)";
+        })
+        mobileOn = true;
+    }
+
+    else {
+        document.querySelector('.burger').style.backgroundImage = "url('images/burger.png')";
+        document.querySelector('.mobile-nav').classList.remove('mobile-nav-bg');
+        document.querySelectorAll('.mobile-nav-links').forEach(link => {
+            link.style.transform = "translate(0, 2000%)";
+        })
+        mobileOn = false;
     }
 })
+
