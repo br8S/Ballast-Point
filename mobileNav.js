5abc889ce87afe87eabaefcd817f613b7396d31f
br8S <56180503+br8S@users.noreply.github.com>
2021-02-25 17:35:18 -0800

    added back button functionality to mobile nav bar

--- mobileNav.js
+++ mobileNav.js
@@ -19,17 +19,22 @@ document.querySelector('.burger').addEventListener('click', () => {
             link.style.transform = "translate(0, 2000%)";
         })
 
+        //makes sure all nested nav options are not displayed when mobile nav is closed
         document.querySelector('.beers-drop-nav').style.display = "none";
         document.querySelector('.beers-drop-nav').classList.remove('mobile-nav-bg');
         document.querySelector('.locations-drop-nav').style.display = "none";
         document.querySelector('.locations-drop-nav').classList.remove('mobile-nav-bg');
         document.querySelector('.about-drop-nav').style.display = "none";
         document.querySelector('.about-drop-nav').classList.remove('mobile-nav-bg');
+
         mobileOn = false;
     }
 })
 
 var mobile_nav_children = document.querySelector('.mobile-nav').children;
+var beers_nav_children = document.querySelector('.beers-drop-nav').children;
+var locations_nav_children = document.querySelector('.locations-drop-nav').children;
+var about_nav_children = document.querySelector('.about-drop-nav').children;
 
 for (var i = 0; i < mobile_nav_children.length; i++) {
     if (i === 0) {
@@ -38,19 +43,39 @@ for (var i = 0; i < mobile_nav_children.length; i++) {
             document.querySelector('.mobile-nav').style.display = "none";
             document.querySelector('.beers-drop-nav').classList.add('mobile-nav-bg');
         })
+
+        beers_nav_children[0].addEventListener('click', () => {
+            document.querySelector('.beers-drop-nav').style.display = "none";
+            document.querySelector('.mobile-nav').style.display = "flex";
+            document.querySelector('.mobile-nav').classList.add('mobile-nav-bg');
+        })
     }
+
     if (i === 1) {
         mobile_nav_children[i].addEventListener('click', () => {
             document.querySelector('.locations-drop-nav').style.display = "flex";
             document.querySelector('.mobile-nav').style.display = "none";
             document.querySelector('.locations-drop-nav').classList.add('mobile-nav-bg');
         })
+
+        locations_nav_children[0].addEventListener('click', () => {
+            document.querySelector('.locations-drop-nav').style.display = "none";
+            document.querySelector('.mobile-nav').style.display = "flex";
+            document.querySelector('.mobile-nav').classList.add('mobile-nav-bg');
+        })
     }
+
     if (i === 2) {
         mobile_nav_children[i].addEventListener('click', () => {
             document.querySelector('.about-drop-nav').style.display = "flex";
             document.querySelector('.mobile-nav').style.display = "none";
             document.querySelector('.about-drop-nav').classList.add('mobile-nav-bg');
+        })
+
+        about_nav_children[0].addEventListener('click', () => {
+            document.querySelector('.about-drop-nav').style.display = "none";
+            document.querySelector('.mobile-nav').style.display = "flex";
+            document.querySelector('.mobile-nav').classList.add('mobile-nav-bg');
         })
     }
 }
